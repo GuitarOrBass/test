09bd048cb64e5e4cd64d600fe6b534e7c4ab3682
GuitarOrBass <AMALFI300@163.com>
2025-03-09 16:28:04 +0800

    add some message and import cv.

--- text_2.py
+++ text_2.py
@@ -1,1 +1,8 @@
-print("Hello world!")
+import cv2 as cv
+# 这里开始读取图像文件
+image = cv.imread("D:/Doucment/Codes/ImageData/img001.jpg", 
+                  flags=cv.IMREAD_GRAYSCALE)
+print("Hello world!") #这是一句测试语句
+'''
+All these codes are using for test git.
+'''
